3d0079c0c95031f375e6f8233ce783ab78af2cb5
T <nguyenquangtruongnqt4@gmail.com>
2025-08-27 19:01:18 +0700

    webcode

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: [
+    "./*/*.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#8C30F5"
+      },
+      fontSize: {
+        H1: ["72px", "98px"],
+        H2: ["48px", "64px"],
+        Lead1: ["18px", "32px"]
+      },
+      spacing: {
+        18: "72px",
+        19: "76px"
+      },
+    },
+  },
+  plugins: [],
+}
